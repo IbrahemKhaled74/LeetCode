2a706f7dfa8ac32ee37cab95fe143374f4f502b5
ibrahem khaled <101200745+IbrahemKhaled74@users.noreply.github.com>
2022-10-24 00:49:17 +0200

    Time: 1 ms (98.78%), Space: 43.2 MB (23.71%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -3,15 +3,15 @@ class Solution {
         int greatest=0;
         List<Boolean> great=new ArrayList<Boolean>();
         for(int j=0 ; j < candies.length ; j++){
+            
             if(candies[j]>greatest){
                 greatest=candies[j];
             }
+            
         }
             
-            
-            
-            
         for (int i =0 ; i <candies.length ; i++ ){
+            
             if(candies[i]+extraCandies >= greatest){
                 great.add(true);
             }else
