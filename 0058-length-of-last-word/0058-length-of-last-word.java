8c0fa469c079345ed3b7f80cc237519d233cb004
Ibrahem Khaled <101200745+IbrahemKhaled74@users.noreply.github.com>
2023-01-27 19:52:12 +0200

    Time: 10 ms (5.06%), Space: 42.3 MB (21.19%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,8 +1,19 @@
 class Solution {
     public int lengthOfLastWord(String s) {
+        // String word=s.trim();
+        // String lastWord = word.substring(word.lastIndexOf(" ")+1);
+        // return lastWord.length();
+        int length = 0 ;
         String word=s.trim();
-        String lastWord = word.substring(word.lastIndexOf(" ")+1);
-        return lastWord.length();
+
+        for(int  i = 0 ; i<word.length() ; i++){
+            if(word.charAt(i) !=' '){
+                length++;
+            }else{
+                length=0;
+            }
+        }
+        return length;
         
     }
 }
