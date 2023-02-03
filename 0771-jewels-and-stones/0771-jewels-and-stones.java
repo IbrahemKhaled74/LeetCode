1dc4c9b20c997d2f9c55102f95a440664508c1a9
Ibrahem Khaled <101200745+IbrahemKhaled74@users.noreply.github.com>
2023-02-03 19:38:22 +0200

    Time: 0 ms (100.00%), Space: 40.4 MB (94.51%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -1,12 +1,17 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
+        // int counter=0;
+        // for(int i = 0 ; i < jewels.length() ; i++){
+        //     for(int j=0 ; j < stones.length() ; j++){
+        //         if(jewels.charAt(i)==stones.charAt(j))counter++;
+        //     }
+        // }
+        // return counter;
+//-------------------------------------------------------------------------
         int counter=0;
-        for(int i = 0 ; i < jewels.length() ; i++){
-            for(int j=0 ; j < stones.length() ; j++){
-                if(jewels.charAt(i)==stones.charAt(j))counter++;
-            }
+        for(int i = 0 ; i < stones.length() ; i++){
+            if(jewels.indexOf(stones.charAt(i))!=-1)counter++;
         }
         return counter;
-        
     }
 }
