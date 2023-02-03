f0c2d0edbb4279240c684b6f7021166064edec7c
Ibrahem Khaled <101200745+IbrahemKhaled74@users.noreply.github.com>
2023-02-03 19:24:38 +0200

    Time: 1 ms (92.19%), Space: 40.7 MB (71.38%) - LeetHub

--- 0771-jewels-and-stones/0771-jewels-and-stones.java
+++ 0771-jewels-and-stones/0771-jewels-and-stones.java
@@ -3,7 +3,7 @@ class Solution {
         int counter=0;
         for(int i = 0 ; i < jewels.length() ; i++){
             for(int j=0 ; j < stones.length() ; j++){
-                if(jewels.charAt(i)==stones.charAt(j) )counter++;
+                if(jewels.charAt(i)==stones.charAt(j))counter++;
             }
         }
         return counter;
