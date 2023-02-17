3f192b6a38b1ee7e38f83bcde143e02564fb0687
Ibrahem Khaled <101200745+IbrahemKhaled74@users.noreply.github.com>
2023-02-17 23:54:37 +0200

    Time: 1 ms (30.67%), Space: 40.3 MB (77.12%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=0 ; i<s.length() ; i++){
             
          if(s.charAt(i)>=65 && s.charAt(i)<=90 ){
-                             word.append( (char) (s.charAt(i)+32) );
+                word.append( (char) (s.charAt(i)+32) );
 
          }else {
                 word.append(s.charAt(i));
