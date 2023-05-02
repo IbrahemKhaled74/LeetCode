4d0a5e27116e5d3c3f298458f614b78ecf6c3506
Ibrahem Khaled <101200745+IbrahemKhaled74@users.noreply.github.com>
2023-05-03 00:18:11 +0300

    Time: 851 ms (77.58%), Space: 0B (100.00%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -1,29 +1,35 @@
 # Write your MySQL query statement below
-SELECT id , 'Root' as type
-FROM Tree 
-WHERE p_id IS NULL
+# SELECT id , 'Root' as type
+# FROM Tree 
+# WHERE p_id IS NULL
 
-UNION
+# UNION
 
-SELECT id , 'Inner' as type
-FROM Tree 
-WHERE id in 
+# SELECT id , 'Inner' as type
+# FROM Tree 
+# WHERE id in 
+# (
+#     SELECT DISTINCT p_id 
+#     FROM Tree
+#     WHERE p_id  IS NOT NULL
+# )
+# AND p_id  IS NOT NULL
+
+# UNION
+
+# SELECT id , 'Leaf' as type
+# FROM Tree 
+# WHERE id not in 
+# (
+#     SELECT DISTINCT p_id 
+#     FROM Tree
+#     WHERE p_id  IS NOT NULL
+# )
+# AND p_id  IS NOT NULL
+
+SELECT id , if
 (
-    SELECT DISTINCT p_id 
-    FROM Tree
-    WHERE p_id  IS NOT NULL
-)
-AND p_id  IS NOT NULL
-
-UNION
-
-SELECT id , 'Leaf' as type
-FROM Tree 
-WHERE id not in 
-(
-    SELECT DISTINCT p_id 
-    FROM Tree
-    WHERE p_id  IS NOT NULL
-)
-AND p_id  IS NOT NULL
+p_id is null , 'Root' , if(id in (select p_id from tree) and p_id is not null,'Inner','Leaf')
+) as type
+from Tree
 
