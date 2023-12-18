3c300124bc64c66f4b988e559bb9e6ff9bf8d453
Ibrahem Khaled <101200745+IbrahemKhaled74@users.noreply.github.com>
2023-12-18 23:39:55 +0200

    Time: 2 ms (91.38%), Space: 45.1 MB (91.87%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.java
@@ -1,18 +1,34 @@
 class Solution {
     public int[] replaceElements(int[] arr) {
-        int size=arr.length;
-        
-        for(int i=0 ; i < size-1 ; i++){
-            int max=0;
-            for(int j=i+1 ; j < size ; j++){
-                
-                max = Math.max(max,arr[j]);        
+//         int size=arr.length;
+//         for(int i=0 ; i < size-1 ; i++){
+//             int max=0;
+//             for(int j=i+1 ; j < size ; j++){
+//                 max = Math.max(max,arr[j]);        
+//             }
+//             arr[i]=max;
+//         }
+//         arr[size-1]=-1;
 
-            }
-            arr[i]=max;
+//         return arr;
+//-----------------------------------------------------------------
+        //Enhancment solution
+        int max= -1 ;
+
+        for(int i=arr.length-1 ; i >= 0 ; i--){
+            if(arr[i] > max){
+                int temp =max;
+                max = arr[i];
+                arr[i]=temp;
+            }else{
+                arr[i]=max;
+            }            
+            
+            
         }
-        arr[size-1]=-1;
+            return arr;
 
-        return arr;
+        
+        
     }
 }
